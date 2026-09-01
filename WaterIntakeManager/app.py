0e0d139b52d5029a0be513c80db585186b7ec9fc
patelkartavya2015-gif <patelkartavya2015@gmail.com>
2026-09-01 17:28:25 +0530

    Add files via upload

---
+++ WaterIntakeManager/app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request
+ 
+app = Flask(__name__)
+ 
+ 
+@app.route("/")
+def home():
+    return render_template("index.html")
+ 
+ 
+@app.route("/track", methods=["POST"])
+def track_water():
+    # Handling form data with request
+    name = request.form["name"]
+    goal = int(request.form["goal"])
+    glasses = int(request.form["glasses"])
+ 
+    remaining = goal - glasses
+ 
+    if glasses >= goal:
+        message = "Great job! You reached your water intake goal today."
+        status = "Goal completed"
+        remaining = 0
+    else:
+        message = "Keep going! You still need to drink more water."
+        status = "Goal not completed"
+ 
+    # Passing data to the HTML template using Jinja2
+    return render_template(
+        "index.html",
+        name=name,
+        goal=goal,
+        glasses=glasses,
+        remaining=remaining,
+        message=message,
+        status=status
+    )
+ 
+ 
+if __name__ == "__main__":
+    app.run(debug=True)
